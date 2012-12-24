a213dda3d114912d2c41c9a769da1efa7001dbee
Jason Hinebaugh <jhinebaugh@acs.org>
2012-12-24 10:16:00 -0500

    started documentation of refactor.js in comments

--- public/scripts/refactor.js
+++ public/scripts/refactor.js
@@ -1,14 +1,21 @@
-var Map, State, StateView, TheUSA,
-    state, usa;
+/*jslint browser: true */
+/*globals _, $, jQuery, Backbone */
 
-Map = {
-    bottom: 23.079732,
-    left: -125.712894,
-    right: -65.419925,
-    scale: 10,
-    top: 49.267805
-}
+var Nation, State,      // Backbone Models
+    Map, StateView,     // Backbone Views
+    state, usa;         // Instances
+/*  
+ -  NOTE: These probably shouldn't be global variables. Rather,
+ -  they should be wrapped in an IIFE.
+*/ 
 
+
+/*  ## Models ##  */
+
+/*
+ -  ### State ###
+ -  A state is both a location on the board and a card in the deck.
+*/
 State = Backbone.Model.extend({
     initialize: function (attributes, options) {
         this.set({parent_map: attributes.parent_map});
@@ -25,6 +32,42 @@ State = Backbone.Model.extend({
         return _.contains(this.get('neighbors'), state);
     }
 });
+
+/*
+ -  ### Nation
+ -  A collection of states. This is the definitive resource that always contains
+ -  exactly one instance of each state. States may belong in the draw pile, discard
+ -  pile, or players' hands, but they are always accessible through their Nation.
+*/
+Nation = Backbone.Collection.extend({
+    model: State,
+/*
+ -  The nation is responsible for loading and setting up its states. Setup can
+ -  only happen after the entire collection is loaded, because it requires each
+ -  state to already know its neighbor states and for all neighbors to be accessible
+ -  via the parent_nation.
+*/
+    initialize: function (attributes, options) {
+        this.add(StateDefaults);
+        _.each(this.models, function (state) {
+            state.set({
+                parent_map: this,
+                neighbors: []
+            });
+            state.setup(); 
+        });
+    }
+});
+
+/*  ## Views ##  */
+
+NationMapView = {
+    bottom: 23.079732,
+    left: -125.712894,
+    right: -65.419925,
+    scale: 10,
+    top: 49.267805
+}
 
 StateMapView = Backbone.View.extend({
     tagName: 'li',
@@ -44,19 +87,6 @@ StateMapView = Backbone.View.extend({
     }
 })
 
-Nation = Backbone.Collection.extend({
-    model: State,
-    initialize: function (attributes, options) {
-        this.add(StateDefaults);
-        _.each(this.models, function (state) {
-            state.set({
-                parent_map: this,
-                neighbors: []
-            });
-            state.setup(); 
-        });
-    }
-});
 
 usa = new Nation;
 usa.each(function (state) {
